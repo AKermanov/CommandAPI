5ed00c1d024ecc749c447bab127eaaee4b1ddea0
Aleksandar Kermanov <a.kermanov@gmail.com>
2021-11-14 17:52:09 +0200

    Change IDE to visual studio

---
+++ src/CommandAPI.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandAPI.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
